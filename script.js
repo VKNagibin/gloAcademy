908e5713c29a70e8e3b1b78a02903d73447b6ca4
Vyacheslav Nagibin <itisnotstupid@gmail.com>
2021-10-01 17:01:37 +0300

    Homework for lesson02

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let title = 'myShortTitle',
-    screens = [1, 2, 3, 4],
-    screenPrice = 1500,
-    fullPrice,
-    adaptive;
+let title = 'The most important project',
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 100,
+    fullPrice = 1500,
+    adaptive = true,
+    rollback = 28;
 
-const rollback = 'd610fb7';
-
-console.log('This title is:', title);
-
-alert('Hello UserName!');
+console.log(typeof title, typeof fullPrice, typeof adaptive);            
+console.log(screens.length);                                            
+console.log('Стоимость вёрстки экранов ' + screenPrice + ' долларов');   
+console.log(screens.toLowerCase().split(", "));                          
+console.log('Процент отката = ' + fullPrice * (rollback / 100)  );   
